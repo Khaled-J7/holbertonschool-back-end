427141c0abea5db0ce65f3232ea610da8d627b91
Khaled Jallouli <6711@holbertonstudents.com>
2024-04-18 22:49:11 +0100

    fix0

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,26 +1,25 @@
 #!/usr/bin/python3
-"""
-show a  TODO list progress.
-"""
+"""Python script to export data in the CSV format."""
 
 import requests
 from sys import argv
 
+
 if __name__ == '__main__':
     main_url = 'https://jsonplaceholder.typicode.com'
+    users_url = main_url + "/users"
+    todos_url = main_url + "/todos"
 
-    todos_url = main_url + "/todos?userId={}".format(argv[1])
-    employee_url = main_url + "/users/{}".format(argv[1])
-
+    users_result = requests.get(users_url).json()
     todos_result = requests.get(todos_url).json()
-    employee_result = requests.get(employee_url).json()
 
-    todo_num = len(todos_result)
-    todo_complete = len([todo for todo in todos_result
-                         if todo.get("completed")])
-    employee = employee_result.get("name")
-    print("Employee {} is done with tasks({}/{}):"
-          .format(employee, todo_complete, todo_num))
-    for todo in todos_result:
-        if (todo.get("completed")):
-            print("\t {}".format(todo.get("title")))
+    for user in users_result:
+        if user['id'] == int(argv[1]):
+            user_name = user['username']
+    with open('{}.csv'.format(argv[1]), 'w') as f:
+        for todo in todos_result:
+            if todo['userId'] == int(argv[1]):
+                task = todo['completed']
+                title = todo['title']
+                f.write(
+                    f"\"{argv[1]}\",\"{user_name}\",\"{task}\",\"{title}\"\n")
